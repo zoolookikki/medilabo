b6a9cb30e96b5eae11cba7fe35cb11c4fc6ff32a
Laurent Cordier <laurent.cordier28@orange.fr>
2025-08-13 11:31:42 +0200

    SP1 : implement Patient controler

---
+++ patient-service/src/main/java/com/medilabo/patient_service/controller/PatientController.java
@@ -0,0 +1,85 @@
+package com.medilabo.patient_service.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.medilabo.patient_service.dto.PatientRequestDTO;
+import com.medilabo.patient_service.dto.PatientResponseDTO;
+import com.medilabo.patient_service.service.PatientService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@RestController 
+@Log4j2
+@RequestMapping("/patients") 
+// Coupler @RequiredArgsConstructor avec des champs final pour rendre les dépendances immuables ==> mieux que @Autowired devenu obsolète.
+@RequiredArgsConstructor
+public class PatientController {
+    
+    private final PatientService patientService;
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getAllPatients() {
+        log.debug("GET/patients");
+        // bonne pratique : si vide, pas de 204 => un 200 avec liste vide.
+        return ResponseEntity.ok(patientService.getAllPatients());
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<PatientResponseDTO> getPatientById(@PathVariable Long id) {
+        log.debug("GET/patients(id),id="+id);
+        Optional<PatientResponseDTO> patient = patientService.getById(id);
+
+        if (patient.isPresent()) {
+            log.info("GET/patients(id) : getById found");
+            return ResponseEntity.ok(patient.get());
+        } else {
+            log.info("GET/patients(id) : getById not found");
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO req){
+        log.debug("POST/patients"+ " : "+req);
+        var createdPatient = patientService.create(req);
+        log.info("POST/patients : create ok");
+        return ResponseEntity
+                .status(HttpStatus.CREATED) // 201 created => preferable to 200 ok.
+                .body(createdPatient); // to respect the standard.
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable Long id, @Valid @RequestBody PatientRequestDTO req) {
+        log.debug("PUT/patients(id),id="+id+ " : "+req);
+        Optional<PatientResponseDTO> updatedPatient = patientService.update(id, req);
+
+        if (updatedPatient.isPresent()) {
+            log.info("PUT/patients : update ok");
+            return ResponseEntity.ok(updatedPatient.get());
+        } else {
+            log.info("PUT/patients : update not ok");
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+/*
+    @GetMapping("/simulate500")
+    public String simulate500() {
+        throw new RuntimeException("Simulation d'erreur interne");
+    }
+*/
+}
+
