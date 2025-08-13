0132eda100374c497fe548d386f4bd8508e151a9
Laurent Cordier <laurent.cordier28@orange.fr>
2025-08-13 11:18:32 +0200

    SP1: implement Patient repository and service

---
+++ patient-service/src/main/java/com/medilabo/patient_service/repository/PatientRepository.java
@@ -0,0 +1,10 @@
+package com.medilabo.patient_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.medilabo.patient_service.model.Patient;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+}
