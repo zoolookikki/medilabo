54e8cca2cacfa67c1866b76b5bef04c73ee9426e
Laurent Cordier <laurent.cordier28@orange.fr>
2025-10-13 15:31:40 +0200

    [SP3] Risk-service : initialization

---
+++ risk-service/src/test/java/com/medilabo/risk_service/RiskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.risk_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
